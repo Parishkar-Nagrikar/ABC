153b892859566357bf8e58f9add72b8a1567a5ae
Parishkar-Nagrikar <p4parish@gmail.com>
2018-07-28 16:26:36 +0530

    Parish : updated sysout

--- src/com/pari/test/Main.java
+++ src/com/pari/test/Main.java
@@ -3,7 +3,8 @@ package com.pari.test;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("abc hello");
+        System.out.print("abc 12345");
+
 	// write your code here
     }
 }
